a84b48fad3e66a09fd7ef99828ae1b9a354ea247
MoueedAhmed <moueedrao@gmail.com>
2020-04-09 16:54:56 +0500

    lecture 26

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {CardList}  from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
 class App extends Component{
   constructor(){
     super();
 
     this.state = {
-      name: 'Moueed'
+      monsters:[],
+      searchField:''
     };
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}))
+  }
+
   render(){
+    const { monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.name}
-          </p>
-          <button onClick = { ()=> this.setState({ name: 'Abdul Moueed' }) }>Click here!</button>
-        </header>
+        <SearchBox placeholder='search monsters' handleChange={e => this.setState({searchField:e.target.value})} />
+        <CardList monsters={filteredMonsters}>
+        </CardList>
       </div>
     );
   }
