389aadcfb174cc6d4933823ce5b0290f6eb07d45
MoueedAhmed <moueedrao@gmail.com>
2020-04-09 23:08:56 +0500

    Updates

---
+++ precache-manifest.1b9149bcb3263f0091337e263ca55e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7587e6cfda77617143692dc5c5a9028e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a61f46c7817d1978ad63",
+    "url": "/monsters-rolodex/static/css/main.1d7995d5.chunk.css"
+  },
+  {
+    "revision": "ad8534d43364924f4cac",
+    "url": "/monsters-rolodex/static/js/2.bedd2c39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bedd2c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61f46c7817d1978ad63",
+    "url": "/monsters-rolodex/static/js/main.9fd0437b.chunk.js"
+  },
+  {
+    "revision": "52e9ddfea8938be86802",
+    "url": "/monsters-rolodex/static/js/runtime-main.e183efee.js"
+  }
+]);
